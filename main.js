b749b19ef40b973bc95d156bd70f97f171baf3ff
Nick Sotula <so2niko@gmail.com>
2022-05-20 20:09:58 +0300

    basic intro to IT

---
+++ main.js
@@ -0,0 +1,17 @@
+//Дані
+/**
+ * boolean - true/false - Так, Ні, Правда, Брехня, логічний тип даних, 
+ * number - 0, 123, -4545, 666.455, NaN. Числа. 
+ * string - 'example 1', "example 2", `example 3` - template string (шаблонні строки)
+ * object - контейнер з даними, 
+ * symbol
+ * null
+ * undefined
+ * big int
+ */
+
+const text = `Lorem ipsum dolor sit amet consectetur ${ 2 + 2 } adipisicing elit. 
+Rem, et quis quia mollitia ab nihil excepturi sit temporibus perspiciatis. 
+Voluptatibus consequuntur rem at neque repellat perferendis consequatur eius recusandae maiores.`;
+
+console.log(text);
